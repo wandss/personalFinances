e2cc510b461ad13ba5738eb47b8c6a8140511be8
WANDERLEY DE SOUZA SILVA <26432@copasa.com.br>
2019-01-07 13:18:20 -0200

    Transform the Django app into an API. Creates a Vue App.

--- financialtrack/financialtrack/urls.py
+++ financialtrack/financialtrack/urls.py
@@ -1,31 +1,31 @@
-"""financialtrack URL Configuration
+"""financialTrack URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
 from django.contrib import admin
-from mainmenu.views import LogoutView
-#from django.contrib.auth import views as auth_views
+from django.urls import path, include, re_path
+from rest_framework_jwt.views import obtain_jwt_token
+from chartData.urls import charts
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^menu/', include('mainmenu.urls', namespace="mainmenu")),
-    url(r'^', include('mainmenu.urls')),
-    url(r'^cadastro/', include('cadastro.urls', namespace="cadastro")),
-    url(r'login/', include('login.urls', namespace="login")),
-    url(r'logout/', LogoutView.as_view(), name='logout' ),
-    #url(r'login/', auth_views.login,
-    #               {'template_name':'login.html'},
-    #               name='login'),
+    re_path('^api/v1/frontend/', include('frontend.urls',namespace='frontend')),
+    re_path('^api/v1/transactions/', include('transactions.urls',
+                                        namespace='transactions')),
+    re_path('^api/v1/charts/', include(charts)),
+    path('api/v1/auth/', obtain_jwt_token),
+    path('api/v1/token/', include('auth_app.urls')),
+    #re_path('^api/v1/auth/',include('auth_app.urls', namespace="authentication")),
+    re_path('^admin', admin.site.urls),
+    re_path(r'[\s\S]*', include('frontend.urls', namespace="app")),
 ]
--- financialtrack/financialtrack/wsgi.py
+++ financialtrack/financialtrack/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for financialtrack project.
+WSGI config for financialTrack project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "financialtrack.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "financialTrack.settings")
 
 application = get_wsgi_application()
